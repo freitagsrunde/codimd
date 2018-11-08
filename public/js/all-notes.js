548116b1df48dc54887452f781e8e9befcae0b83
Gereon Dusella <git@gereondusella.de>
2018-11-08 13:02:23 +0100

    Users are now able to view all available non-private notes in a view
    Signed-off-by: Gereon Dusella <git@gereondusella.de>

---
+++ public/js/all-notes.js
@@ -0,0 +1,74 @@
+/* eslint-env browser, jquery */
+/* global serverurl, moment */
+
+// import store from 'store'
+import S from 'string'
+import LZString from 'lz-string'
+
+import {
+  checkNoteIdValid,
+  encodeNoteId
+} from './utils'
+
+// import {
+//   checkIfAuth
+// } from './lib/common/login'
+//
+// import {
+//   urlpath
+// } from './lib/config'
+
+// function getAllNotes (callback) {
+//   $.get(`${serverurl}/allNotes`)
+//     .done(data => {
+//       if (data.allNotes) {
+//         callback(data.allNotes)
+//       }
+//     })
+//     .fail((xhr, status, error) => {
+//       console.error(xhr.responseText)
+//     })
+// }
+
+export function parseServerAllNotes (list, callback) {
+  $.get(`${serverurl}/allNotes`)
+    .done(data => {
+      if (data.allNotes) {
+        parseAllNotes(list, data.allNotes, callback)
+      }
+    })
+    .fail((xhr, status, error) => {
+      console.error(xhr.responseText)
+    })
+}
+
+function parseAllNotes (list, allNotes, callback) {
+  if (!callback) return
+  else if (!list || !allNotes) callback(list, allNotes)
+  else if (allNotes && allNotes.length > 0) {
+    for (let i = 0; i < allNotes.length; i++) {
+      // // migrate LZString encoded id to base64url encoded id
+      try {
+        let id = LZString.decompressFromBase64(allNotes[i].id)
+        if (id && checkNoteIdValid(id)) {
+          allNotes[i].id = encodeNoteId(id)
+        }
+      } catch (err) {
+        console.error(err)
+      }
+
+      // let id = allNotes[i].id
+      // parse time to timestamp and fromNow
+      const timestamp = (typeof allNotes[i].updatedAt === 'number' ? moment(allNotes[i].updatedAt) : moment(allNotes[i].updatedAt, 'MMMM Do YYYY, h:mm:ss a'))
+      allNotes[i].timestamp = timestamp.valueOf()
+      allNotes[i].fromNow = timestamp.fromNow()
+      allNotes[i].time = timestamp.format('llll')
+      // prevent XSS
+      allNotes[i].text = S(allNotes[i].text).escapeHTML().s
+      allNotes[i].tags = (allNotes[i].tags && allNotes[i].tags.length > 0) ? S(allNotes[i].tags).escapeHTML().s.split(',') : []
+      // add to list
+      if (allNotes[i].id && list.get('id', allNotes[i].id).length === 0) { list.add(allNotes[i]) }
+    }
+  }
+  callback(list, allNotes)
+}
